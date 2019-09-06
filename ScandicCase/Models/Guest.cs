d7df4500535980630374bbcb0184d53fc10ef507
Edward Aschan <edward@aschan.org>
2019-09-06 15:58:28 +0200

    Moved guest validation to guest through extension method

--- ScandicCase/Models/Guest.cs
+++ ScandicCase/Models/Guest.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ScandicCase.Models
+﻿namespace ScandicCase.Models
 {
     public class Guest
     {
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public string Title { get; set; }
-
-
     }
 }
---
+++ ScandicCase/Models/ValidationExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ScandicCase.Models
+{
+	internal static class ValidationExtensions
+	{
+		public static bool IsGuestValidForCountry(this Guest guest, Country countryCode)
+		{
+			// First just checking that the guest have a name
+			var nameValid = !string.IsNullOrWhiteSpace(guest.FirstName)  && !string.IsNullOrWhiteSpace(guest.LastName);
+
+			// Then checking if we must have a title and if so it it is given
+			var titleValid = countryCode != Country.DE || (countryCode == Country.DE && !string.IsNullOrWhiteSpace(guest.Title));
+
+			return nameValid && titleValid;
+		}
+	}
+}
